2c14a03e301250be71dddbe6de6fb5fd61913f1e
bradley augstein <bradley.augstein@finastra.co.uk>
2022-03-19 09:08:07 +0000

    Add Prophet (performs poorly on current data set)

--- MachineLearning/Python/DeepLearningFX/Hackathon/Hackathon.py
+++ MachineLearning/Python/DeepLearningFX/Hackathon/Hackathon.py
@@ -3,6 +3,53 @@ import plotly.express as px #alternative charting function
 import plotly.io as pio #graphs do not work in Spyder
 pio.renderers.default='browser'
 
+
+#-----------------------------------------
+#folio indicators
+#-----------------------------------------
+folio_df = pd.read_csv('Data/Portfolio_data.csv',engine = 'python')
+
+print(folio_df.tail(40))
+print(folio_df.info()) #now see float 64 (aside from time serie)
+
+#todo - filter for specific position / take largest empty position
+folio_df=folio_df[folio_df['GroupByCriteriaValues']=='AMERICAN DOLLAR']
+folio_df=folio_df[folio_df['Indicator']=='Asset Fx Delta']
+
+#sum all positions 
+#folio_df=folio_df[folio_df['GroupByCriteriaValues']!=''] #doesn't filter
+
+folio_plot=pd.melt(folio_df, id_vars=['Date'], value_vars=['Result'])
+
+fig = px.line(folio_plot, x='Date', y='value', color='variable')
+# Show plot 
+#fig.show()
+
+
+from prophet import Prophet
+from pylab import rcParams
+import matplotlib.pyplot as plt
+#-----------------------------------------
+#Prophet - Model folio indicators 
+#-----------------------------------------
+folio_df_2 = folio_df[['Date','Result']]
+item1=folio_df_2
+item1.columns = ['ds','y']
+item1.y = item1.y.astype('float')
+item1.ds = item1.ds
+item1.sort_values('ds')
+rcParams['figure.figsize'] = 20,5
+plt.plot(item1.ds, item1.y)
+plt.xlabel("Date")
+plt.ylabel("MarketValue ($Millions)")
+ph = Prophet()
+ph.fit(item1)
+forecast1=ph.predict(item1)
+figure = ph.plot(forecast1)
+figure.show()
+
+
+
 #-----------------------------------------
 #trade indicators
 #-----------------------------------------
@@ -35,27 +82,17 @@ fig.show()
 #-----------------------------------------
 #Prophet - Model trade indicators 
 #-----------------------------------------
+item1=trade_df_all
+item1.columns = ['ds','y']
+item1.y = item1.y.astype('float')
+item1.ds = item1.ds.astype('datetime64')
 
-
-
-
-#-----------------------------------------
-#folio indicators
-#-----------------------------------------
-folio_df = pd.read_csv('Data/Portfolio_data.csv',engine = 'python')
-
-print(folio_df.tail(40))
-print(folio_df.info()) #now see float 64 (aside from time serie)
-
-#todo - filter for specific position / take largest empty position
-folio_df=folio_df[folio_df['GroupByCriteriaValues']=='AMERICAN DOLLAR']
-folio_df=folio_df[folio_df['Indicator']=='Asset Fx Delta']
-
-#sum all positions 
-#folio_df=folio_df[folio_df['GroupByCriteriaValues']!=''] #doesn't filter
-
-folio_plot=pd.melt(folio_df, id_vars=['Date'], value_vars=['Result'])
-
-fig = px.line(folio_plot, x='Date', y='value', color='variable')
-# Show plot 
-fig.show()
+rcParams['figure.figsize'] = 20,5
+plt.plot(item1.ds, item1.y)
+plt.xlabel("Date")
+plt.ylabel("MarketValue ($Millions)")
+ph = Prophet()
+ph.fit(item1)
+forecast1=ph.predict(item1)
+figure = ph.plot(forecast1)
+figure.show()
