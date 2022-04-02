167a5cd11bebe247444b23c7616bbf06fe1539ec
bradley augstein <bradley.augstein@finastra.co.uk>
2022-04-02 09:48:18 +0100

    Updates from Heloise includes : - 40 day lookback function - Increased epoch and batch size - Adds forward prediction

--- MachineLearning/Python/DeepLearningFX/Hackathon/Hackathon_LSTM_folio.py
+++ MachineLearning/Python/DeepLearningFX/Hackathon/Hackathon_LSTM_folio.py
@@ -10,34 +10,34 @@ import openpyxl
 #%matplotlib inline
 from datetime import date
 
-#Reading dataset
-data_set = pd.read_csv('Data/Portfolio_data2.csv',engine = 'python')
-data_set=data_set[data_set['GroupByCriteriaValues']=='AMERICAN DOLLAR']
-data_set=data_set[data_set['Indicator']=='Asset Fx Delta']
+#-----------------------------------------
+#folio indicators
+#-----------------------------------------
 
-data_set=pd.melt(data_set, id_vars=['Date'], value_vars=['Result'])
-#Data frame
-df = data_set['value']
+print('Reading data ....')
+data_set = pd.read_excel('Data/Portfolio_data3_convert.xlsx',na_values='ND')
+print('Done')
+print(data_set.tail(40))
+print(data_set.info()) #now see float 64 (aside from time serie)
+
+data_set=data_set[data_set['More Columns.GroupByLevel1Value']=='AMERICAN DOLLAR']
+target_column = 'More Columns.Asset Fx Delta in %'
+df = data_set[target_column]
 #Preprocessing data set
 df = np.array(df).reshape(-1,1)
 
-'''df['dt']=pd.to_datetime(data_set['Date'],format='%d/%m/%Y')
-df['week'] = data_set['dt'].dt.week 
-df['day'] = data_set['dt'].dt.day
-df['month'] = data_set['dt'].dt.month
-data_set['dayofweek'] = data_set['dt'].dt.dayofweek'''
+#-----------------------------------------
+#preparing training and testing data
+#-----------------------------------------
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 df = scaler.fit_transform(df)
-
+plt.plot(df)
 
 #Training and test sets
-train = df[:260]
-test = df[260:]
-
-print(train.shape)
-print(test.shape)
+train = df[:350]
+test = df[350:]
 
 def get_data(data, look_back):
   datax, datay = [],[]
@@ -46,48 +46,124 @@ def get_data(data, look_back):
     datay.append(data[i+look_back,0])
   return np.array(datax) , np.array(datay)
 
-look_back = 1
+# Used to predict 1 day in the future:
+def get_dataTest(data, look_back):
+  datax, datay = [],[]
+  for i in range(len(data)-look_back):
+    datax.append(data[i:(i+look_back),0])
+    datay.append(data[i+look_back,0])
+  j = len(data)-look_back
+  datax.append(data[j:(j+look_back),0])
+  return np.array(datax) , np.array(datay)
 
+look_back = 40
+
+#Create training inputs [samples, timesteps] and outputs [samples]
 x_train , y_train = get_data(train, look_back)
-print(x_train.shape)
-print(y_train.shape)
 
-x_test , y_test = get_data(test,look_back)
-print(x_test.shape)
-print(y_test.shape)
+#Create testing inputs [samples, timesteps] and outputs [samples]
+x_test , y_test = get_dataTest(test,look_back)
 
+n_features = 1
 #Processing train and test sets for LSTM model
-x_train = x_train.reshape(x_train.shape[0],x_train.shape[1], 1)
-x_test = x_test.reshape(x_test.shape[0],x_test.shape[1], 1)
-print(x_train.shape)
-print(x_test.shape)
+#Reshape inputs from [samples, timesteps] into [samples, timesteps, features]
+x_train = x_train.reshape(x_train.shape[0],x_train.shape[1], n_features)
+x_test = x_test.reshape(x_test.shape[0],x_test.shape[1], n_features)
+
+#-----------------------------------------
+#LSTM
+#-----------------------------------------
 
 #Defining the LSTM model
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,LSTM
+from tensorflow.keras.layers import Dense,LSTM,Bidirectional
 
-n_features=x_train.shape[1]
+#Stacked LSTM:
+timesteps=x_train.shape[1]
 model=Sequential()
-model.add(LSTM(100,activation='relu',input_shape=(n_features,1)))
+model.add(LSTM(100,activation='relu', return_sequences=True, input_shape=(timesteps,n_features)))
+model.add(LSTM(100,activation='relu'))
 model.add(Dense(1))
-
-print(model.summary())
 
 #Compiling
 model.compile(optimizer='adam', loss = 'mse')
 
 #Training
-model.fit(x_train,y_train, epochs = 20, batch_size=1)
+model.fit(x_train,y_train, epochs = 250, batch_size=32)
 
+#-----------------------------------------
+#LSTM testing
+#-----------------------------------------
+
+#Prediction using the trained model
+scaler.scale_
 y_pred = model.predict(x_test)
+plt.plot(y_pred)
+
+#Processing test shape
+y_testPlot = np.array(y_test).reshape(-1,1)
+y_testPlot = scaler.inverse_transform(y_testPlot)
+y_predPlot = scaler.inverse_transform(y_pred)
 
 #Visualizing the results
 plt.figure(figsize=(10,5))
 plt.title('LSTM',fontsize = 20)
-plt.plot(y_test , label = 'Actual', color = "darkblue",linestyle="dotted")
-plt.plot(y_pred , label = 'Predicted', color = "red")
+plt.plot(y_testPlot , label = 'Actual', color = "darkblue",linestyle="dotted")
+plt.plot(y_predPlot , label = 'Predicted', color = "red")
+#plt.xlabel('Date',fontsize = 15)
+#plt.ylabel('Asset Value FX Delta in %',fontsize = 15)
 plt.rc('legend', fontsize = 15)
-plt.xlim(0.0,160)
+plt.xlim(0.0)
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.grid(True)
+plt.legend()
+
+#-----------------------------------------
+#Extending prediction
+#-----------------------------------------
+
+y_predTotal=y_pred
+newMem0 = x_test[-1]
+#remove first value and add last prediction value
+newMem = np.append(newMem0, y_pred[-1])
+for i in range(30):
+    newMem = np.delete(newMem, 0)
+    newMem = newMem.reshape(1,newMem.shape[0], n_features)
+    #predict new future value:
+    y_stepPred=model.predict(newMem)
+    #add new value to final prediction:
+    y_predTotal=np.append(y_predTotal, y_stepPred)
+    newMem = np.append(newMem, y_stepPred)
+y_predTotal=y_predTotal.reshape(y_predTotal.shape[0], n_features)
+y_predTotalPlot = scaler.inverse_transform(y_predTotal)
+
+#Visualizing the results
+plt.figure(figsize=(10,5))
+plt.title('LSTM - 30 days prediction',fontsize = 20)
+plt.plot(y_testPlot , label = 'Actual', color = "darkblue",linestyle="dotted")
+plt.plot(y_predTotalPlot , label = 'Predicted', color = "red")
+#plt.xlabel('Date',fontsize = 15)
+#plt.ylabel('Asset Value FX Delta in %',fontsize = 15)
+plt.rc('legend', fontsize = 15)
+plt.xlim(0.0)
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.grid(True)
+plt.legend()
+
+#-----------------------------------------
+#Checking the training results
+#-----------------------------------------
+
+y_trainPred=model.predict(x_train)
+#Visualizing the results
+plt.figure(figsize=(10,5))
+plt.title('Training data results',fontsize = 20)
+plt.plot(y_train , label = 'Actual', color = "darkblue",linestyle="dotted")
+plt.plot(y_trainPred , label = 'Predicted', color = "red")
+plt.rc('legend', fontsize = 15)
+plt.xlim(0.0)
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 plt.grid(True)
