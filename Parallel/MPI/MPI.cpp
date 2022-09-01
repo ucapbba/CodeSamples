dc83a4497a318ec9fe3ba544dd3bc23844f918f6
bradley augstein <bradley.augstein@finastra.co.uk>
2022-09-01 08:44:44 +0100

    Fran-183 - add .bat to run MPI (required for parallel run)

--- Parallel/MPI/MPI.cpp
+++ Parallel/MPI/MPI.cpp
@@ -9,10 +9,22 @@ int main()
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
     // Get the rank of the process
-    int my_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    // Print the message
-    printf("Hello World! My rank is %d\n", my_rank);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Print off a hello world message
+    printf("Hello world from processor %s, rank %d out of %d processors\n",
+        processor_name, world_rank, world_size);
+
     // Finalize the MPI environment.
     MPI_Finalize();
 }
---
+++ Parallel/MPI/Run_MPI.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+cd x64/Release
+mpiexec -n 5 MPI.exe
+
+PAUSE
