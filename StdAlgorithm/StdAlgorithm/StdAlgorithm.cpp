b305c505fc7e0041ab8f841e4072ceac027cf662
bradley augstein <bradley.augstein@finastra.co.uk>
2022-09-22 13:42:42 +0100

    Sample for sorting vector of custom classes

--- StdAlgorithm/StdAlgorithm/StdAlgorithm.cpp
+++ StdAlgorithm/StdAlgorithm/StdAlgorithm.cpp
@@ -6,8 +6,42 @@
 #include <numeric>
 #include <vector>
 
+bool myfunction(int i, int j) { return (i > j); }
+
+class initCondVars {
+public:
+	initCondVars(int a, int b);
+	int variable1;
+	int variable2;
+};
+initCondVars::initCondVars(int a, int b)
+{
+	variable1 = a;
+	variable2 = b;
+}
+bool myfunctionInit(initCondVars i, initCondVars j) { return (i.variable2 > j.variable2); }
 int main()
 {
+
+
+#pragma region sort_test
+	initCondVars a1(5, 1);
+	initCondVars a2(4, 2);
+	initCondVars a3(3, 3);
+	initCondVars a4(2, 4);
+	initCondVars a5(1, 5);
+	std::vector<initCondVars> sortVector;
+	sortVector.push_back(a1);
+	sortVector.push_back(a2);
+	sortVector.push_back(a3);
+	sortVector.push_back(a4);
+	sortVector.push_back(a5);
+
+	std::sort(sortVector.begin(), sortVector.end(), myfunctionInit);
+#pragma endregion	
+	
+#pragma region other_std
+
 	std::vector<double> inVector;
 	std::vector<double> outVector(5);
 	inVector.push_back(1.0);
@@ -15,6 +49,9 @@ int main()
 	inVector.push_back(3.0);
 	inVector.push_back(4.0);
 	inVector.push_back(5.0);
+
+	//simple sort
+	std::sort(inVector.begin(), inVector.end(), myfunction);
 	double y = 2;
 	std::transform(inVector.begin(), inVector.end(), outVector.begin(), [y](double x) { return x * y; });
 
@@ -24,6 +61,7 @@ int main()
 	std::transform(outVector.begin(), outVector.end(), outVector.begin(), outVector.begin(), std::multiplies<double>()); //multiply of pairs 
 	double outVecSum = std::accumulate(outVector.begin() + 1, outVector.end(), 0.);
 
+#pragma endregion
     std::cout << "Hello World!\n";
 }
 
