5b84aadfb0b617818bfba901b23fbd6aefee6180
bradley augstein <bradley.augstein@finastra.co.uk>
2022-10-31 15:55:15 +0000

    Remove columns from table using removeat

--- DevExpress/2022 Sample/DXApplication1/DXApplication2/Form1.Designer.cs
+++ DevExpress/2022 Sample/DXApplication1/DXApplication2/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace DXApplication2
 {
@@ -40,6 +41,7 @@ namespace DXApplication2
             this.dockPanel2_Container = new DevExpress.XtraBars.Docking.ControlContainer();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).BeginInit();
             this.dockPanel1.SuspendLayout();
@@ -94,13 +96,21 @@ namespace DXApplication2
             this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(213, 25);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.AutoSize, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(375, 124);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.AutoSize, 2F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(375, 10);
             this.tableLayoutPanel1.TabIndex = 1;
-            this.tableLayoutPanel1.Controls.Add(new Label { Text = "Instrument   ", Anchor = AnchorStyles.Left, AutoSize = false }, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(new Label { Text = "Folio        ", Anchor = AnchorStyles.Left, AutoSize = false }, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(new Label { Text = "Folio Path   ", Anchor = AnchorStyles.Left, AutoSize = false }, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(new Label { Text = "Instrument   ", Anchor = AnchorStyles.Left, AutoSize = true }, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(new Label { Text = "Folio        ", Anchor = AnchorStyles.Left, AutoSize = true }, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(new Label { Text = "Folio Path   ", Anchor = AnchorStyles.Left, AutoSize = true }, 2, 0);
+            //this.tableLayoutPanel1.RowCount = 1;
+            //this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            Size maxSize = new Size(this.Size.Width,this.Size.Height);
+            this.tableLayoutPanel1.MaximumSize = maxSize;
+            //this.tableLayoutPanel1.AutoScrollMargin = new Size(this.Size.Width, this.Size.Height);
+            //this.tableLayoutPanel1.AutoScroll = true;
             // 
             // textBox1
             // 
@@ -145,6 +155,17 @@ namespace DXApplication2
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(500, 235);
+            this.button2.Name = "clear table";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "clear table";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // checkBox1
             // 
@@ -164,6 +185,7 @@ namespace DXApplication2
             this.ClientSize = new System.Drawing.Size(778, 473);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dockPanel2);
             this.Controls.Add(this.dockPanel1);
             this.Name = "Form1";
@@ -188,6 +210,7 @@ namespace DXApplication2
         private DevExpress.XtraBars.Docking.DockPanel dockPanel1;
         private DevExpress.XtraBars.Docking.ControlContainer dockPanel1_Container;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox textBox2;
