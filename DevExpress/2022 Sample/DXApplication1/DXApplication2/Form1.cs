96a57ff4ea941593fc58b5f7a85d81c7b6447fad
bradley augstein <bradley.augstein@finastra.co.uk>
2022-10-31 15:55:57 +0000

    Remove columns from table using removeat

--- DevExpress/2022 Sample/DXApplication1/DXApplication2/Form1.cs
+++ DevExpress/2022 Sample/DXApplication1/DXApplication2/Form1.cs
@@ -16,7 +16,23 @@ namespace DXApplication2
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.tableLayoutPanel1.RowCount > 0)
+            {
+                for (int i = 0; i < this.tableLayoutPanel1.RowCount * 3; i++)
+                {
+                    this.tableLayoutPanel1.Controls.RemoveAt(3); //this is horrific but it works 
+                    
+                }
+                this.tableLayoutPanel1.RowCount = 0;
+                this.tableLayoutPanel1.Update();
+            }
+
+
+        }
+
+            private void button1_Click(object sender, EventArgs e)
         {
             //update panel 1
             textBox1.Text = "updated";
@@ -25,7 +41,13 @@ namespace DXApplication2
             this.tableLayoutPanel1.Controls.Add(new Label { Text = "My Folio", Anchor = AnchorStyles.Left, AutoSize = true }, 1, this.tableLayoutPanel1.RowCount);
             this.tableLayoutPanel1.Controls.Add(new Label { Text = "My Folio Path", Anchor = AnchorStyles.Left, AutoSize = true }, 2, this.tableLayoutPanel1.RowCount);
             this.tableLayoutPanel1.Refresh();
-     
+
+            if (this.tableLayoutPanel1.RowCount > 5)
+            {
+                Size minSize = new Size(this.Size.Width + 500, this.Size.Height);
+                this.tableLayoutPanel1.AutoScrollMinSize = minSize;
+            }
+
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -37,6 +59,44 @@ namespace DXApplication2
             else
             {
                 this.textBox2.Text = "reverted";
+            }
+        }
+
+        public static class TableLayoutHelper
+        {
+            public static void RemoveArbitraryRow(TableLayoutPanel panel, int rowIndex)
+            {
+                if (rowIndex >= panel.RowCount)
+                {
+                    return;
+                }
+
+                // delete all controls of row that we want to delete
+                for (int i = 0; i < panel.ColumnCount; i++)
+                {
+                    var control = panel.GetControlFromPosition(i, rowIndex);
+                    panel.Controls.Remove(control);
+                }
+
+                // move up row controls that comes after row we want to remove
+                for (int i = rowIndex + 1; i < panel.RowCount; i++)
+                {
+                    for (int j = 0; j < panel.ColumnCount; j++)
+                    {
+                        var control = panel.GetControlFromPosition(j, i);
+                        if (control != null)
+                        {
+                            panel.SetRow(control, i - 1);
+                        }
+                    }
+                }
+
+                var removeStyle = panel.RowCount - 1;
+
+                if (panel.RowStyles.Count > removeStyle)
+                    panel.RowStyles.RemoveAt(removeStyle);
+
+                panel.RowCount--;
             }
         }
     }
