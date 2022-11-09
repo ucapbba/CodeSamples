acd1b2285857b23746f6297afff8ed3a202e1c1e
bradley augstein <bradley.augstein@finastra.co.uk>
2022-11-09 08:44:10 +0000

    GridControl sample from https://github.com/DevExpress-Examples/how-to-bind-gridcontrol-to-a-collection-of-business-objects-t604166/tree/17.2.3%2B/CS

---
+++ DevExpress/2022 Sample/DXApplication1/GridBoundToRuntimeCreatedData/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GridBoundToRuntimeCreatedData {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar2 = new DevExpress.XtraBars.Bar();
+            this.btnClearPayment = new DevExpress.XtraBars.BarButtonItem();
+            this.btnSetPayment = new DevExpress.XtraBars.BarButtonItem();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 22);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(566, 275);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar2});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.btnClearPayment,
+            this.btnSetPayment});
+            this.barManager1.MainMenu = this.bar2;
+            this.barManager1.MaxItemId = 2;
+            // 
+            // bar2
+            // 
+            this.bar2.BarName = "Main menu";
+            this.bar2.DockCol = 0;
+            this.bar2.DockRow = 0;
+            this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.btnClearPayment),
+            new DevExpress.XtraBars.LinkPersistInfo(this.btnSetPayment)});
+            this.bar2.OptionsBar.MultiLine = true;
+            this.bar2.OptionsBar.UseWholeRow = true;
+            this.bar2.Text = "Main menu";
+            // 
+            // btnClearPayment
+            // 
+            this.btnClearPayment.Caption = "Clear Payment";
+            this.btnClearPayment.Id = 0;
+            this.btnClearPayment.Name = "btnClearPayment";
+            this.btnClearPayment.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnClearPayment_ItemClick);
+            // 
+            // btnSetPayment
+            // 
+            this.btnSetPayment.Caption = "Set Payment";
+            this.btnSetPayment.Id = 1;
+            this.btnSetPayment.Name = "btnSetPayment";
+            this.btnSetPayment.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnSetPayment_ItemClick);
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(566, 22);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 297);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(566, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 22);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 275);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(566, 22);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 275);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 297);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar2;
+        private DevExpress.XtraBars.BarButtonItem btnClearPayment;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.BarButtonItem btnSetPayment;
+    }
+}
+
---
+++ DevExpress/2022 Sample/DXApplication1/GridBoundToRuntimeCreatedData/Form1.cs
@@ -0,0 +1,176 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridBoundToRuntimeCreatedData {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            gridControl1.DataSource = DataHelper.GetData(10);
+            // The grid automatically creates columns for the public fields found in the data source. 
+            // Calling the gridView1.PopulateColumns method is not required unless the gridView1.OptionsBehavior.AutoPopulateColumns is disabled
+            
+            // Create a ComboBox editor that shows available companies in the Company column
+            RepositoryItemComboBox riComboBox = new RepositoryItemComboBox();
+            riComboBox.Items.AddRange(DataHelper.companies);
+            gridControl1.RepositoryItems.Add(riComboBox);
+            gridView1.Columns["CompanyName"].ColumnEdit = riComboBox;
+
+            // Specify a different null value text presentation for the Image column
+            gridView1.Columns["Image"].RealColumnEdit.NullText = "[load image]";
+
+            //Highlight the RequiredDate cells that match a certain condition.
+            GridFormatRule gridFormatRule = new GridFormatRule();
+            FormatConditionRuleValue formatConditionRuleValue = new FormatConditionRuleValue();
+            gridFormatRule.Column = gridView1.Columns["RequiredDate"];
+            formatConditionRuleValue.PredefinedName = "Red Bold Text";
+            formatConditionRuleValue.Condition = FormatCondition.Greater;
+            formatConditionRuleValue.Value1 = DateTime.Today;
+            gridFormatRule.Rule = formatConditionRuleValue;
+            gridFormatRule.ApplyToRow = false;
+            gridView1.FormatRules.Add(gridFormatRule);
+
+            gridView1.BestFitColumns();
+        }
+
+        private void btnClearPayment_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+            //Change a cell value at the data source level to see the INotifyPropertyChanged interface in action.
+            gridView1.CloseEditor();
+            Record rec = gridView1.GetFocusedRow() as Record;
+            if (rec == null) return;
+            rec.Value = 0;
+        }
+
+        private void btnSetPayment_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+            //Change a cell value at the grid level
+            gridView1.SetFocusedRowCellValue("Value", 999);
+        }
+    }
+
+    public class Record : INotifyPropertyChanged {
+        public Record() {
+        }
+        int id;
+        public int ID {
+            get { return id; }
+            set {
+                if (id != value) {
+                    id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
+        string text;
+        [DisplayName("Company")]
+        public string CompanyName {
+            get { return text; }
+            set {
+                if (text != value) {
+                    if (string.IsNullOrEmpty(value))
+                        throw new Exception();
+                    text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        Nullable<decimal> val;
+        [DataType(DataType.Currency)]
+        [DisplayName("Payment")]
+        public Nullable<decimal> Value {
+            get { return val; }
+            set {
+                if (val != value) {
+                    val = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        DateTime dt;
+        [DisplayFormat(DataFormatString = "d")]
+        public DateTime RequiredDate {
+            get { return dt; }
+            set {
+                if (dt != value) {
+                    dt = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        bool state;
+        public bool Processed {
+            get { return state; }
+            set {
+                if (state != value) {
+                    state = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        Image image;
+        public Image Image {
+            get { return image; }
+            set {
+                if (image != value) {
+                    image = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public override string ToString() {
+            return string.Format("ID = {0}, Text = {1}", ID, CompanyName);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DataHelper {
+
+        public static string[] companies = new string[] { "Hanari Carnes", "Que Delícia", "Romero y tomillo", "Mère Paillarde",
+            "Comércio Mineiro", "Reggiani Caseifici", "Maison Dewey" };
+
+        public static Image[] images = new Image[] {
+            global::GridBoundToRuntimeCreatedData.Properties.Resources.palette_16x16,
+            global::GridBoundToRuntimeCreatedData.Properties.Resources.viewonweb_16x16,
+            global::GridBoundToRuntimeCreatedData.Properties.Resources.design_16x16,
+            global::GridBoundToRuntimeCreatedData.Properties.Resources.piestylepie_16x16,
+            global::GridBoundToRuntimeCreatedData.Properties.Resources.alignhorizontaltop2_16x16,
+            null
+        };
+
+        public static BindingList<Record> GetData(int count) {
+            BindingList<Record> records = new BindingList<Record>();
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++) {
+                int n = rnd.Next(10);
+                records.Add(new Record() {
+                    ID = i + 100,
+                    CompanyName = companies[i % companies.Length],
+                    RequiredDate = DateTime.Today.AddDays(n - 5),
+                    Value = i % 2 == 0 ? (i + 1) * 123 : i * 231,
+                    Processed = i % 2 == 0,
+                    Image = images[i % images.Length],
+                });
+            };
+            return records;
+        }
+    }
+}
