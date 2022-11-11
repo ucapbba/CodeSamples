709f45590db9df4c042b39b0d0e754c177d75d29
bradley augstein <bradley.augstein@finastra.co.uk>
2022-11-11 16:42:53 +0000

    Prevent moving dockPanel1 outside GUI with AllowFloating

--- DevExpress/2022 Sample/DXApplication1/DXApplication3_GridView_Docking/Form1.Designer.cs
+++ DevExpress/2022 Sample/DXApplication1/DXApplication3_GridView_Docking/Form1.Designer.cs
@@ -1,4 +1,8 @@
 ﻿
+using DevExpress.Utils.Menu;
+using DevExpress.XtraBars.Docking;
+using DevExpress.XtraGrid.Localization;
+
 namespace DXApplication3_GridView_Docking
 {
     partial class Form1
@@ -31,16 +35,16 @@ namespace DXApplication3_GridView_Docking
         {
             this.components = new System.ComponentModel.Container();
             this.dockManager1 = new DevExpress.XtraBars.Docking.DockManager(this.components);
-            this.dockPanel1 = new DevExpress.XtraBars.Docking.DockPanel();
-            this.dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
             this.dockPanel2 = new DevExpress.XtraBars.Docking.DockPanel();
             this.dockPanel2_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+            this.dockPanel1 = new DevExpress.XtraBars.Docking.DockPanel();
+            this.dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).BeginInit();
+            this.dockPanel2.SuspendLayout();
             this.dockPanel1.SuspendLayout();
             this.dockPanel1_Container.SuspendLayout();
-            this.dockPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
@@ -49,8 +53,8 @@ namespace DXApplication3_GridView_Docking
             // 
             this.dockManager1.Form = this;
             this.dockManager1.RootPanels.AddRange(new DevExpress.XtraBars.Docking.DockPanel[] {
-            this.dockPanel1,
-            this.dockPanel2});
+            this.dockPanel2,
+            this.dockPanel1});
             this.dockManager1.TopZIndexControls.AddRange(new string[] {
             "DevExpress.XtraBars.BarDockControl",
             "DevExpress.XtraBars.StandaloneBarDockControl",
@@ -65,49 +69,54 @@ namespace DXApplication3_GridView_Docking
             "DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl",
             "DevExpress.XtraBars.ToolbarForm.ToolbarFormControl"});
             // 
-            // dockPanel1
-            // 
-            this.dockPanel1.Controls.Add(this.dockPanel1_Container);
-            this.dockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Top;
-            this.dockPanel1.ID = new System.Guid("c17854b2-a67c-400a-8b2d-9dbf3007876b");
-            this.dockPanel1.Location = new System.Drawing.Point(0, 0);
-            this.dockPanel1.Name = "dockPanel1";
-            this.dockPanel1.OriginalSize = new System.Drawing.Size(200, 173);
-            this.dockPanel1.Size = new System.Drawing.Size(632, 173);
-            this.dockPanel1.Text = "dockPanel1";
-            // 
-            // dockPanel1_Container
-            // 
-            this.dockPanel1_Container.Controls.Add(this.gridControl1);
-            this.dockPanel1_Container.Location = new System.Drawing.Point(3, 26);
-            this.dockPanel1_Container.Name = "dockPanel1_Container";
-            this.dockPanel1_Container.Size = new System.Drawing.Size(626, 143);
-            this.dockPanel1_Container.TabIndex = 0;
-            // 
             // dockPanel2
             // 
             this.dockPanel2.Controls.Add(this.dockPanel2_Container);
-            this.dockPanel2.Dock = DevExpress.XtraBars.Docking.DockingStyle.Bottom;
+            this.dockPanel2.Dock = DevExpress.XtraBars.Docking.DockingStyle.Top;
+            this.dockPanel2.FloatVertical = true;
             this.dockPanel2.ID = new System.Guid("e50aaaab-f822-455b-8dff-39260c1da84a");
-            this.dockPanel2.Location = new System.Drawing.Point(0, 173);
+            this.dockPanel2.Location = new System.Drawing.Point(0, 0);
             this.dockPanel2.Name = "dockPanel2";
             this.dockPanel2.OriginalSize = new System.Drawing.Size(200, 105);
             this.dockPanel2.Size = new System.Drawing.Size(632, 105);
             this.dockPanel2.Text = "dockPanel2";
+            this.dockPanel2.Options.AllowFloating = false;
             // 
             // dockPanel2_Container
             // 
-            this.dockPanel2_Container.Location = new System.Drawing.Point(3, 27);
+            this.dockPanel2_Container.Location = new System.Drawing.Point(3, 26);
             this.dockPanel2_Container.Name = "dockPanel2_Container";
             this.dockPanel2_Container.Size = new System.Drawing.Size(626, 75);
             this.dockPanel2_Container.TabIndex = 0;
             // 
+            // dockPanel1
+            // 
+            this.dockPanel1.Controls.Add(this.dockPanel1_Container);
+            this.dockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Bottom;
+            this.dockPanel1.FloatVertical = true;
+            this.dockPanel1.ID = new System.Guid("c17854b2-a67c-400a-8b2d-9dbf3007876b");
+            this.dockPanel1.Location = new System.Drawing.Point(0, 105);
+            this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.OriginalSize = new System.Drawing.Size(200, 173);
+            this.dockPanel1.Size = new System.Drawing.Size(632, 173);
+            this.dockPanel1.Text = "dockPanel1";
+            this.dockPanel1.Options.AllowFloating = false;
+            // 
+            // dockPanel1_Container
+            // 
+            this.dockPanel1_Container.Controls.Add(this.gridControl1);
+            this.dockPanel1_Container.Location = new System.Drawing.Point(3, 27);
+            this.dockPanel1_Container.Name = "dockPanel1_Container";
+            this.dockPanel1_Container.Size = new System.Drawing.Size(626, 143);
+            this.dockPanel1_Container.TabIndex = 0;
+            // 
             // gridControl1
             // 
-            this.gridControl1.Location = new System.Drawing.Point(24, 16);
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(382, 110);
+            this.gridControl1.Size = new System.Drawing.Size(626, 143);
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -116,20 +125,26 @@ namespace DXApplication3_GridView_Docking
             // 
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            //this.gridView1.OptionsCustomization.AllowQuickHideColumns = false;
+
+          
+
+
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 278);
-            this.Controls.Add(this.dockPanel2);
             this.Controls.Add(this.dockPanel1);
+            this.Controls.Add(this.dockPanel2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).EndInit();
+            this.dockPanel2.ResumeLayout(false);
             this.dockPanel1.ResumeLayout(false);
             this.dockPanel1_Container.ResumeLayout(false);
-            this.dockPanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
@@ -145,6 +160,7 @@ namespace DXApplication3_GridView_Docking
         private DevExpress.XtraBars.Docking.ControlContainer dockPanel1_Container;
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        public event PopupMenuShowingEventHandler PopupMenuShowing;
     }
 }
 
