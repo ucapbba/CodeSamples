c73ff159b09ed433845452dfd29127283ceade33
bradley augstein <bradley.augstein@finastra.co.uk>
2022-11-17 15:25:52 +0000

    Docked GridView example

--- DevExpress/2022 Sample/DXApplication1/DXApplication3_GridView_Docking/Form1.Designer.cs
+++ DevExpress/2022 Sample/DXApplication1/DXApplication3_GridView_Docking/Form1.Designer.cs
@@ -1,7 +1,10 @@
 ﻿
 using DevExpress.Utils.Menu;
+using DevExpress.XtraBars;
 using DevExpress.XtraBars.Docking;
 using DevExpress.XtraGrid.Localization;
+using System;
+using System.Windows.Forms;
 
 namespace DXApplication3_GridView_Docking
 {
@@ -41,12 +44,20 @@ namespace DXApplication3_GridView_Docking
             this.dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.btnDoStuff = new DevExpress.XtraBars.BarButtonItem();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar2 = new DevExpress.XtraBars.Bar();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
             ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).BeginInit();
             this.dockPanel2.SuspendLayout();
             this.dockPanel1.SuspendLayout();
             this.dockPanel1_Container.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
             this.SuspendLayout();
             // 
             // dockManager1
@@ -75,12 +86,12 @@ namespace DXApplication3_GridView_Docking
             this.dockPanel2.Dock = DevExpress.XtraBars.Docking.DockingStyle.Top;
             this.dockPanel2.FloatVertical = true;
             this.dockPanel2.ID = new System.Guid("e50aaaab-f822-455b-8dff-39260c1da84a");
-            this.dockPanel2.Location = new System.Drawing.Point(0, 0);
+            this.dockPanel2.Location = new System.Drawing.Point(0, 20);
             this.dockPanel2.Name = "dockPanel2";
+            this.dockPanel2.Options.AllowFloating = false;
             this.dockPanel2.OriginalSize = new System.Drawing.Size(200, 105);
             this.dockPanel2.Size = new System.Drawing.Size(632, 105);
             this.dockPanel2.Text = "dockPanel2";
-            this.dockPanel2.Options.AllowFloating = false;
             // 
             // dockPanel2_Container
             // 
@@ -97,10 +108,10 @@ namespace DXApplication3_GridView_Docking
             this.dockPanel1.ID = new System.Guid("c17854b2-a67c-400a-8b2d-9dbf3007876b");
             this.dockPanel1.Location = new System.Drawing.Point(0, 105);
             this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.Options.AllowFloating = false;
             this.dockPanel1.OriginalSize = new System.Drawing.Size(200, 173);
             this.dockPanel1.Size = new System.Drawing.Size(632, 173);
             this.dockPanel1.Text = "dockPanel1";
-            this.dockPanel1.Options.AllowFloating = false;
             // 
             // dockPanel1_Container
             // 
@@ -120,17 +131,87 @@ namespace DXApplication3_GridView_Docking
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
+            this.gridControl1.Click += new System.EventHandler(this.gridControl1_Click);
+            this.gridControl1.BackColor = System.Drawing.Color.White;
+            ToolTip tip = new ToolTip();
+            this.gridControl1.ToolTipController = new DevExpress.Utils.ToolTipController();
+            this.gridControl1.ToolTipController.SetToolTip(gridControl1, "click for position info");
+
+ 
+
+                //tip.SetToolTip(tableInstance.Controls[totalControls + i], "click to copy column");
+                //tableInstance.Controls[totalControls + i].Click += new System.EventHandler(panelInstance.name_click1);
+
             // 
             // gridView1
             // 
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
-            //this.gridView1.OptionsCustomization.AllowQuickHideColumns = false;
-
-          
-
-
-
+            // 
+            // btnDoStuff
+            // 
+            this.btnDoStuff.Caption = "Do Stuff";
+            this.btnDoStuff.Id = 0;
+            this.btnDoStuff.Name = "btnDoStuff";
+            this.btnDoStuff.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnDoStuff_ItemClick);
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar2});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.btnDoStuff});
+            this.barManager1.MainMenu = this.bar2;
+            this.barManager1.MaxItemId = 2;
+            // 
+            // bar2
+            // 
+            this.bar2.BarName = "Main menu";
+            this.bar2.DockCol = 0;
+            this.bar2.DockRow = 0;
+            this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.btnDoStuff)});
+            this.bar2.OptionsBar.MultiLine = true;
+            this.bar2.OptionsBar.UseWholeRow = true;
+            this.bar2.Text = "Main menu";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(632, 20);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 278);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(632, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 20);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 258);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(632, 20);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 258);
             // 
             // Form1
             // 
@@ -139,6 +220,10 @@ namespace DXApplication3_GridView_Docking
             this.ClientSize = new System.Drawing.Size(632, 278);
             this.Controls.Add(this.dockPanel1);
             this.Controls.Add(this.dockPanel2);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).EndInit();
@@ -147,9 +232,13 @@ namespace DXApplication3_GridView_Docking
             this.dockPanel1_Container.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
+
+
 
         #endregion
 
@@ -161,6 +250,13 @@ namespace DXApplication3_GridView_Docking
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         public event PopupMenuShowingEventHandler PopupMenuShowing;
+        private DevExpress.XtraBars.BarButtonItem btnDoStuff;
+        private DevExpress.XtraBars.Bar bar2;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private BarDockControl barDockControlLeft;
+        private BarDockControl barDockControlBottom;
+        private BarDockControl barDockControlRight;
     }
 }
 
--- DevExpress/2022 Sample/DXApplication1/DXApplication3_GridView_Docking/Form1.cs
+++ DevExpress/2022 Sample/DXApplication1/DXApplication3_GridView_Docking/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.XtraBars;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,102 @@ namespace DXApplication3_GridView_Docking
         public Form1()
         {
             InitializeComponent();
+           // gridControl1.DataSource = DataHelper.GetBasicData();
+        }
+
+        private void btnDoStuff_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            gridControl1.DataSource = DataHelper.GetData(10);
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+            if(gridControl1.DataSource==null)
+                gridControl1.DataSource = DataHelper.GetData(10);
         }
     }
+
+
+
+    public class Record
+    {
+        public Record()
+        {
+        }
+        int positionId;
+        public int posID
+        {
+            get { return positionId; }
+            set
+            {
+                if (positionId != value)
+                {
+                    positionId = value;
+                }
+            }
+        }
+
+        string text;
+        [DisplayName("InstrumentName")]
+        public string InstrumentName
+        {
+            get { return text; }
+            set
+            {
+                if (text != value)
+                {
+                    if (string.IsNullOrEmpty(value))
+                        throw new Exception();
+                    text = value;
+                }
+            }
+        }
+
+        public static BindingList<Record> GetData(int count)
+        {
+            BindingList<Record> records = new BindingList<Record>();
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int n = rnd.Next(10);
+                records.Add(new Record()
+                {
+                    positionId = i + 100,
+                });
+            };
+            return records;
+        }
+    }
+
+    public class DataHelper
+    {
+        public static string[] instruments = new string[] { "Apple", "Orangle", "Romero y tomillo", "ABN Amro" };
+        public static BindingList<Record> GetData(int count)
+        {
+
+
+            BindingList<Record> records = new BindingList<Record>();
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int n = rnd.Next(10);
+                records.Add(new Record()
+                {
+                    posID = i + 100,
+                    InstrumentName = instruments[i % instruments.Length],
+                });
+            };
+            return records;
+        }
+        public static BindingList<string> GetBasicData()
+        {
+
+
+            BindingList<string> records = new BindingList<string>();
+          
+                records.Add("Hello2 World");
+            return records;
+        }
+    }
+
 }
