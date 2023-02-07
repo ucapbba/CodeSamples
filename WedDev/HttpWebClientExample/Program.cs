28a792acdb3fc9ed98e7d9dccea3d59754f9c590
bradley augstein <bradley.augstein@finastra.co.uk>
2023-02-07 18:11:25 +0000

    GEN-6 - sample to access http client

---
+++ WedDev/HttpWebClientExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpWebClientExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+            var url = "http://localhost:8081/";
+            var content = await client.GetStringAsync(url);
+            Console.WriteLine(content);
+            var result = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+            Console.WriteLine(result);
+        }
+    }
+}
