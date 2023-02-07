b2e780377a1a8baad645b5a497de117ba93c12dc
bradley augstein <bradley.augstein@finastra.co.uk>
2023-02-07 19:13:35 +0000

    Gen-6 - example post posting from C# to Python web service

---
+++ WedDev/HttpWebClientExample/FlaskWebServerExample.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+from flask import Flask, jsonify, request
+ 
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
+
+# main driver function
+if __name__ == '__main__': 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
+    
--- WedDev/HttpWebClientExample/Program.cs
+++ WedDev/HttpWebClientExample/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HttpWebClientExample
@@ -9,11 +11,19 @@ namespace HttpWebClientExample
         static async Task Main(string[] args)
         {
             using var client = new HttpClient();
-            var url = "http://localhost:8081/";
+            var url = "http://127.0.0.1:5000/incomes";
             var content = await client.GetStringAsync(url);
             Console.WriteLine(content);
             var result = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
             Console.WriteLine(result);
+            
+            //post
+            var person = new string("{description:lottery,amount:1000.0}"); //todo
+            var json = JsonConvert.SerializeObject(person);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, data);
+            var result2 = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result2);
         }
     }
 }
