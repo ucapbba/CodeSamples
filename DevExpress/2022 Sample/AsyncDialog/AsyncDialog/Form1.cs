34e731f9001039af66d4fd248f67ff19e8603bbb
bradley augstein <bradley.augstein@finastra.co.uk>
2023-02-27 09:55:43 +0000

    Fran-218 - add asyncContext.post GUI update sample

--- DevExpress/2022 Sample/AsyncDialog/AsyncDialog/Form1.cs
+++ DevExpress/2022 Sample/AsyncDialog/AsyncDialog/Form1.cs
@@ -17,35 +17,67 @@ namespace AsyncDialog
         public Form1()
         {
             InitializeComponent();
+            _syncContext = SynchronizationContext.Current;
         }
-        private WindowsFormsSynchronizationContext context;
+        //private WindowsFormsSynchronizationContext context;
+        private readonly SynchronizationContext _syncContext;
         private void Form1_Load(object sender, EventArgs e)
         {
             // Instead of invoke, Create a new instance of WindowsFormsSynchronizationContext
-            context = new WindowsFormsSynchronizationContext();
+            //context = new WindowsFormsSynchronizationContext();
 
         }
 
         private async void simpleButton1_Click(object sender, EventArgs e)
         {
-            await Task.Run(() => MyMethod()); //asynchronious 
+           // await Task.Run(() => InvokeMethod()); //asynchronious 
+            aSyncContextPost();
         }
 
-        private async void MyMethod()
+        private void aSyncContextPost()
+        {
+            // Start a background task
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                _syncContext.Post(state2 =>
+                {
+                    textEdit2.Text = "calcualtion in progress.....";
+                }, null);
+
+                // Simulate some work
+                 Thread.Sleep(2000);
+
+                // Post a message to the UI thread to update the label
+                _syncContext.Post(state2 =>
+                {
+                    textEdit2.Text = "Background task completed!";
+                }, null);
+            });
+        }
+
+        private async void InvokeMethod()
         {
             Invoke((Action)delegate //ensures update to GUI done with UI master / control thread
             {
                 this.simpleButton1.Enabled = false;
                 textEdit2.Text = "calcualtion in progress.....";
             });
-            await Task.Delay(4000); 
+            await Task.Delay(4000);
             //Thread.Sleep(2000); Delay means in principle the thread can be used for other work
 
             Invoke((Action)delegate //ensures update to GUI done with UI master / control thread
             {
                 this.simpleButton1.Enabled = true;
                 textEdit2.Text = "calcualtion complete";
-            });
+            }); 
+
+
+            //we can achieve the same result by using asynccontext post :
+            //_syncContext.Post(state2 =>
+            //{
+            //    textEdit2.Text = "Background task completed!";
+            //}, null);
+
         }
 
     }
