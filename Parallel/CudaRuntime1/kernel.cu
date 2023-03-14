fe736cae5decce81648e836197a5124b663ce2c9
bradley augstein <bradley.augstein@finastra.co.uk>
2023-03-14 20:43:18 +0000

    Simple sample showing incompatibility of CUDA project and boost

---
+++ Parallel/CudaRuntime1/kernel.cu
@@ -0,0 +1,16 @@
+﻿
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+//#include <thrust/device_vector.h>
+//#include <thrust/reduce.h>
+//#include <thrust/functional.h>
+#include <boost/numeric/odeint.hpp>
+//#include <boost/numeric/odeint/external/thrust/thrust.hpp>
+//#include <boost/random/mersenne_twister.hpp>
+//#include <boost/random/uniform_real.hpp>
+//#include <boost/random/variate_generator.hpp>
+//#include <stdio.h>
+int main(int arc, char* argv[])
+{
+	return 0;
+}
