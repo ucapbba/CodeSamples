f1d996d8ed2fc1de0af71e046befdaa4eaf03584
bradley augstein <bradley.augstein@finastra.co.uk>
2023-10-30 18:59:47 +0000

    .NET MAUI example - replace simple hello world with Grid view for shopping list (data bindings not implemented fully)

---
+++ .NET MAUI/MyFirstApp/MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace MauiApp1.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string text;
+
+        public string Text
+        {
+            get => text;
+            set
+            {
+               text = value;
+                OnPropertChanged(nameof(Text));
+
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertChanged(string name)=>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
