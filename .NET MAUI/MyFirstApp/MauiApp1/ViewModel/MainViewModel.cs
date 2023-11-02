a307a6b7e563f65334474c4c1e8bbc865d4ada48
bradley augstein <bradley.augstein@finastra.co.uk>
2023-11-02 21:15:18 +0000

    MAUI App - add REST calls

--- .NET MAUI/MyFirstApp/MauiApp1/ViewModel/MainViewModel.cs
+++ .NET MAUI/MyFirstApp/MauiApp1/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace MauiApp1.ViewModel
@@ -9,11 +10,20 @@ namespace MauiApp1.ViewModel
 
         public MainViewModel() 
         {
-
-
-            Items = new ObservableCollection<string>();
+            ReloadFromServer();
         }
+        public void ReloadFromServer()
+        {
+            List<TodoModel> currentItems = myRESTAPI.Get();
+            int size = currentItems.Count;
+            ObservableCollection<string> collection = new ObservableCollection<string>();
+            foreach (TodoModel item in currentItems)
+            {
+                collection.Add(item.name);
+            }
 
+            Items = collection;
+        }
         [ObservableProperty]
         ObservableCollection<string>   _items;
 
@@ -23,12 +33,19 @@ namespace MauiApp1.ViewModel
         [RelayCommand]
         void Add()
         {
-            if(string.IsNullOrEmpty(Text))
+           
+            if (string.IsNullOrEmpty(Text))
             {
                 return;
             }
+            if(myRESTAPI.success==false)
+                ReloadFromServer();
 
             Items.Add(Text);
+            List<TodoModel> currentItemsTask = myRESTAPI.Get();
+            List<TodoModel> currentItems = currentItemsTask;
+            int size = currentItems.Count;
+            myRESTAPI.Post(size + 1, Text);
             Text = string.Empty;
         }
 
@@ -36,6 +53,9 @@ namespace MauiApp1.ViewModel
         void Delete(string s)
         {
             if(Items.Contains(s)) { Items.Remove(s); }
+            List <TodoModel> currentItems = myRESTAPI.Get();
+            int? id = currentItems.Where(a => a.name == s).FirstOrDefault().id;
+            myRESTAPI.delete((int)id);
         }
 
     }
