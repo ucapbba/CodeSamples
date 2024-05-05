7a73776a2a5f3893b767ced724b3f59184f5a3d8
Bradley-Augstein_finastra <Bradley.Augstein@dh.com>
2024-05-05 21:39:52 +0100

    Improve sample documentation

--- UnitTest/ProjectToTest/Dll1/Class.h
+++ UnitTest/ProjectToTest/Dll1/Class.h
@@ -1,8 +1,48 @@
 #pragma once
+/**
+ * Detailed information.......
+ * 
+ * 
+ * 
+ * more detailed info........
+ * 
+ * 
+ * 
+ * etc.........
+ * @brief ClassToTest used for unit testing.  
+
+ */
 class ClassToTest
-{
+{  
+
+private:
+
+    /**
+    * @brief Sample function for doxygen documentation
+    * @param a some variable passed to the function 
+    * @return returns the variable 1
+    *
+    */
+    double myWellDocumentedFucntion(double a) { return a; }
+
 public:
+    /**
+    * Create a new ClassToTest object .
+    * @brief Default constructor.
+    * @see myWellDocumentedFucntion(double a)
+    * @see myfuncworking()
+    * @see myfuncbroken()
+    */
+    ClassToTest() {};
+    /**
+    * @brief Function for working unit test
+    * @return returns a double
+    */
 	double myfuncworking() { return 1.2; }
+    /**
+    * @brief Originally a function for the failing unit test
+    * @return returns a double
+    */
 	double myfuncbroken() { return 1.3; }
 };
 
