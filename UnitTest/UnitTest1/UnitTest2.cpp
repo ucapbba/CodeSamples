1b0c88dd2a8212fafefcdac6dbfb2dff1f2b081f
Bradley-Augstein_finastra <Bradley.Augstein@dh.com>
2024-06-10 10:56:28 +0100

    typo in yml

---
+++ UnitTest/UnitTest1/UnitTest2.cpp
@@ -0,0 +1,56 @@
+#include "CppUnitTest.h"
+#include "../ProjectToTest/Dll1/Class.h"
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include "../../submodules//json/include/nlohmann/json.hpp"
+#include <tuple>
+#include "HelperFunctions.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTestFile2)
+	{
+	public:
+
+
+		HelperFunctions help;
+		TEST_METHOD(CanReadEnvVariable)
+		{
+			char* file = help.GetEnvVariable("INPUT_FILE");
+			if (!file)
+			{
+				std::cout << "filename not found, will use default" << std::endl;
+				Assert::AreEqual(1, 0);
+			}
+
+			std::cout << "filename = " << file << std::endl;
+			Assert::AreEqual(1, 1);
+		}
+
+		TEST_METHOD(CanOpenFileTest)
+		{
+			bool canOpen = help.CanOpenFile();
+			Assert::AreEqual(true, canOpen);
+		}
+
+		TEST_METHOD(TestInputFile)
+		{
+			int param1 = 20;
+			int param2 = 30;
+			std::string param3 = "some_value";
+			std::string nested_param1 = "abcd";
+			int nested_param2 = 100;
+
+			auto [p1, p2, p3, p4, np1, np2] = help.GetValuesFromJSON();
+
+			Assert::AreEqual(param1, p1);
+			Assert::AreEqual(param2, p2);
+			Assert::AreEqual(param3, p3);
+			Assert::AreEqual(nested_param1, np1);
+			Assert::AreEqual(nested_param2, np2);
+
+		}
+	};
+}
